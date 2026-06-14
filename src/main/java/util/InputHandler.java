3034ebc9b02eef34c2792996182f0515bb084d3e
KOKOCI-star <naufaldzikrullah90@gmail.com>
2026-06-14 21:14:18 +0700

    +InputHandler

--- src/main/java/util/InputHandler.java
+++ src/main/java/util/InputHandler.java
@@ -1,4 +1,10 @@
 package util;
 
 public class InputHandler {
+    public static boolean isStringValid(String input) {
+        return input != null && !input.trim().isEmpty();
+    }
+    public static boolean isPositive(double value) {
+        return value >= 0;
+    }
 }
