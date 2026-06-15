d7621fce042b730159a31dc91441ba327094e9df
KOKOCI-star <naufaldzikrullah90@gmail.com>
2026-06-16 06:01:08 +0700

    Entry Point & Launching Stage

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class Main extends Application {
@@ -27,3 +26,8 @@ public class Main extends Application {
             e.printStackTrace();
         }
     }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
