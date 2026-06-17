5d572fe60f46cbe8f9a5bc1b3e7bbcb0e5e5c69f
KOKOCI-star <naufaldzikrullah90@gmail.com>
2026-06-17 20:57:37 +0700

    +logika control input

--- src/main/java/controller/KnowledgeController.java
+++ src/main/java/controller/KnowledgeController.java
@@ -1,4 +1,57 @@
 package controller;
 
+import model.KnowledgeRepository;
+import model.Putusan;
+import util.InputHandler;
+
 public class KnowledgeController {
+
+    private KnowledgeRepository repository;
+
+    public KnowledgeController() {
+        this.repository = new KnowledgeRepository();
+    }
+
+    /**
+     * Menerima array string, memvalidasi, lalu menyimpannya ke memori.
+     * Asumsi indeks rawData:
+     * 0: No. Perkara, 1: Pengadilan, 2: Tanggal, 3: Nama, 4: Umur, 5: Jenis Narkotika,
+     * 6: Berat BB, 7: Pasal, 8: Peran, 9: Vonis Hukuman, 10: Vonis Denda, 11: Nama Hakim
+     */
+    public boolean tambahPutusan(String[] rawData) {
+
+        if (rawData == null || rawData.length < 12) {
+            return false;
+        }
+
+        if (!InputHandler.isStringValid(rawData[0]) || !InputHandler.isStringValid(rawData[3])) {
+            return false;
+        }
+
+        int umur = InputHandler.validasiInt(rawData[4]);
+        double beratBB = InputHandler.validasiDouble(rawData[6]);
+        int vonisHukuman = InputHandler.validasiInt(rawData[9]);
+        double vonisDenda = InputHandler.validasiDouble(rawData[10]);
+
+        if (umur == -1 || beratBB == -1.0 || vonisHukuman == -1 || vonisDenda == -1.0) {
+            System.err.println("Gagal memproses data: Tipe data angka tidak valid.");
+            return false;
+        }
+
+        if (!InputHandler.isPositive(beratBB) || !InputHandler.isPositive(vonisDenda)) {
+            System.err.println("Gagal memproses data: Nilai angka tidak boleh minus.");
+            return false;
+        }
+
+        Putusan putusanBaru = new Putusan(
+                rawData[0], rawData[1], rawData[2], rawData[3],
+                umur, rawData[5], beratBB, rawData[7],
+                rawData[8], vonisHukuman, vonisDenda, rawData[11]
+        );
+
+        repository.simpan(putusanBaru);
+
+        System.out.println("Data perkara " + rawData[0] + " berhasil diorkestrasi dan disimpan.");
+        return true;
+    }
 }
