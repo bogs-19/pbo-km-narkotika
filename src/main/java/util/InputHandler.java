555e405d6a27fd38ba165273f46d5cee83324c65
KOKOCI-star <naufaldzikrullah90@gmail.com>
2026-06-17 20:49:58 +0700

    +validasi input dan exception

--- src/main/java/util/InputHandler.java
+++ src/main/java/util/InputHandler.java
@@ -1,10 +1,27 @@
 package util;
 
 public class InputHandler {
+
     public static boolean isStringValid(String input) {
         return input != null && !input.trim().isEmpty();
     }
     public static boolean isPositive(double value) {
         return value >= 0;
+    }
+
+    public static int validasiInt(String value) {
+        try {
+            return Integer.parseInt(value);
+        } catch (NumberFormatException e) {
+            return -1;
+        }
+    }
+
+    public static double validasiDouble(String value) {
+        try {
+            return Double.parseDouble(value);
+        } catch (NumberFormatException e) {
+            return -1;
+        }
     }
 }
