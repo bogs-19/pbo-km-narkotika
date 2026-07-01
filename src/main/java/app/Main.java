00e87492a1ae98bf7e4b5770791755c2ffc6fd15
radhitama19 <baguspratama5000@gmail.com>
2026-07-01 20:16:53 +0700

    Memperbaiki struktur kode

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -13,24 +13,16 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
-        // === JURUS BYPASS CLASSPATH INTELLIJ ===
-        // Mengarahkan aplikasi untuk membuka LoginView pertama kali
         File fxmlFile = new File("src/main/resources/fxml/LoginView.fxml");
         URL fxmlUrl = fxmlFile.toURI().toURL();
         Parent root = FXMLLoader.load(fxmlUrl);
-        // =======================================
-
-        // Sesuaikan dimensi dengan desain LoginView kamu
         Scene scene = new Scene(root, 900, 600);
 
         primaryStage.setTitle("KMS Command Center - Narkotika");
         primaryStage.setScene(scene);
 
-        // Kunci ukuran layar agar desain login yang rapi tidak rusak ditarik user
         primaryStage.setResizable(false);
 
-        // Memastikan jendela selalu muncul pas di tengah layar monitor
         primaryStage.centerOnScreen();
 
         primaryStage.show();
